5a510eb011f7d6cfb60ee360374c8e3f43362f4e
Robert Miller <millerrobert369@aol.com>
2022-04-15 17:52:11 -0400

    - Change SwiftUI List to ScrollView, LazyVStack and ForEach - Hide navigationBarTitle and use text element for the title in RecipeListView

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -16,36 +16,52 @@ struct RecipeListView: View {
         
         //Create a list with the data from the recipes array
         NavigationView {
-            List(model.recipes) { r in
+            
+            VStack(alignment: .leading) {
                 
-                //Use NavigationLink to create the arrows for each item so we can go to different views for each recipe
-                NavigationLink(
-                    //RecipeDetailView needs the property from RecipeListView (recipe:) and the iterator(r) using in the List above
-                    destination: RecipeDetailView(recipe: r) ,
-                    label: {
-                        HStack(spacing: 20){
+                //Text on top of view
+                Text("All Recipes")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 40)
+                
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipes) { r in
                             
-                            //Display the image from the assets folder
-                            Image(r.image)
-                                //Allow the image to be resizable
-                                .resizable()
-                                //Scale the image to fill the frame
-                                .scaledToFill()
-                                //Create a frame with a specified width and height
-                                .frame(width: 50, height: 50, alignment: .center
-                                )
-                                //Clip the overflowing portion of the image
-                                .clipped()
-                                //Round the corners of the image
-                                .cornerRadius(5)
-                            //Display the text of the food
-                            Text(r.name)
+                            //Use NavigationLink to create the arrows for each item so we can go to different views for each recipe
+                            NavigationLink(
+                                //RecipeDetailView needs the property from RecipeListView (recipe:) and the iterator(r) using in the List above
+                                destination: RecipeDetailView(recipe: r) ,
+                                label: {
+                                    HStack(spacing: 20){
+                                        
+                                        //Display the image from the assets folder
+                                        Image(r.image)
+                                            //Allow the image to be resizable
+                                            .resizable()
+                                            //Scale the image to fill the frame
+                                            .scaledToFill()
+                                            //Create a frame with a specified width and height
+                                            .frame(width: 50, height: 50, alignment: .center
+                                            )
+                                            //Clip the overflowing portion of the image
+                                            .clipped()
+                                            //Round the corners of the image
+                                            .cornerRadius(5)
+                                        //Display the text of the food
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                }
+                            })
+                            //Create and HStack to display the data horizontally. "spacing" dictates the space between each piece of data. I.E. the image and the text
+                        }
                     }
-                })
-                //Create and HStack to display the data horizontally. "spacing" dictates the space between each piece of data. I.E. the image and the text
+                }
             }
-            //Create a title that will display in the Navigation Bar
-            .navigationBarTitle("All Recipes")
+            //Remove the extra white space at the top of the view where the navigation title would normally be
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
     }
 }
